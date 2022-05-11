636bd0badde17ba89a78c70bb5b2cbc8d0761f9e
Weston McNamara <westonfmcnamara@gmail.com>
2022-05-11 10:09:29 -0400

    Added javadoc comments

--- src/CGMath.java
+++ src/CGMath.java
@@ -3,25 +3,37 @@
 */
 public class CGMath
 {
-    /*
-        Returns true if a point is on the right side of a line defined by a and b
-    */
+    /** Returns true if a point is on the right side of a line defined by a and b
+     * @param a First point defining a line
+     * @param b Second point defining a line
+     * @param c Point to test
+     * @return True if the point is on the right side of the line
+     */
     public static boolean edgeFunction(Vector2 a, Vector2 b, Vector2 c)
     {
         return ((c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x) >= 0);
     }
 
-    /*
-        Checks if a point is inside a triangle defined like tp1, tp2 and tp3
-    */
+
+    /** Checks if a point is inside a triangle defined like tp1, tp2 and tp3
+     * @param tp1 The first point defining a triangle
+     * @param tp2 The second point defining a triangle
+     * @param tp3 The third point defining a triangle
+     * @param pToTest The point to test if its inside the triangle
+     * @return True if the pToTest point is inside the triangle
+     */
     public static boolean pointInsideTriangle(Vector2 tp1, Vector2 tp2, Vector2 tp3, Vector2 pToTest)
     {
         return edgeFunction(tp1, tp2, pToTest) && edgeFunction(tp2, tp3, pToTest) && edgeFunction(tp3, tp1, pToTest);
     }
 
-    /*
-       Checks if a point is inside a triangle defined like tp1, tp2 and tp3
-   */
+    /** Checks if a point is inside a triangle defined like tp1, tp2 and tp3
+     * @param tp1 The first point defining a triangle
+     * @param tp2 The second point defining a triangle
+     * @param tp3 The third point defining a triangle
+     * @param pToTest The point to test if its inside the triangle
+     * @return True if the pToTest point is inside the triangle
+     */
     public static boolean pointInsideTriangle(Vector3 tp1, Vector3 tp2, Vector3 tp3, Vector3 pToTest)
     {
         return edgeFunction(tp1.toVector2(), tp2.toVector2(), pToTest.toVector2()) &&
@@ -29,9 +41,12 @@ public class CGMath
                 edgeFunction(tp3.toVector2(), tp1.toVector2(), pToTest.toVector2());
     }
 
-    /*
-        Converts normalized device coordinates to screen space coordinates
-    */
+    /** Converts normalized device coordinates to screen space coordinates
+     * @param ndcCoord Converts a normalized device coordinate to a screen space coordinate
+     * @param screenWidth The width of the screen
+     * @param screenHeight The height of the screen
+     * @return A screen space coordinate
+     */
     public static Vector2 NDCToScreenSpace(Vector2 ndcCoord, int screenWidth, int screenHeight)
     {
         Vector2 result = new Vector2(0, 0);
@@ -41,9 +56,13 @@ public class CGMath
         return result;
     }
 
-    /*
-        Converts screen spaces coordinates to normalized device coordinates
+    /** Converts screen spaces coordinates to normalized device coordinates
+     * @param screenPnt Screen space coordinate (x, y pixel position)
+     * @param width Width of the screen that the screenpoint lies in
+     * @param height Height of the screen that the screenpoint lies in
+     * @return A coordinate in normalized device coordinate space
      */
+
     public static Vector2 ScreenSpaceToNDC(Vector2 screenPnt, int width, int height)
     {
         Vector2 ndc = new Vector2(0, 0);
@@ -54,8 +73,12 @@ public class CGMath
         return ndc;
     }
 
-    /*
-        Converts screen spaces coordinates to normalized device coordinates
+
+    /** Converts screen spaces coordinates to normalized device coordinates
+     * @param screenPnt Screen space coordinate to convert
+     * @param width Width of the screen
+     * @param height Height of the screen
+     * @return The screen space coordinate in normalized device coordinate
      */
     public static Vector3 ScreenSpaceToNDC(Vector3 screenPnt, int width, int height)
     {
@@ -68,9 +91,13 @@ public class CGMath
         return ndc;
     }
 
-    /*
-        Converts normalized device coordinates to screen space coordinates
-    */
+
+    /** Converts normalized device coordinates to screen space coordinates
+     * @param ndcCoords Normalized device coordinate to be converted
+     * @param screenWidth Width of the screen
+     * @param screenHeight Height of the screen
+     * @return The normalized device coordinate in screen space
+     */
     public static Vector2[] NDCToScreenSpace(Vector2[] ndcCoords, int screenWidth, int screenHeight)
     {
         Vector2[] scPts = new Vector2[ndcCoords.length];
